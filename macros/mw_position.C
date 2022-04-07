a8be6e059facb5e88986923ec7f68360cc35901c
Tobias Jenegger HOME <jeni94@live.de>
2022-04-07 12:27:37 +0200

    pictures and mw macro added

---
+++ macros/mw_position.C
@@ -0,0 +1,55 @@
+//insert all headers
+#include "TTree.h"
+#include "TFile.h"
+#include <math.h>
+#include <string>
+#include <iostream>
+
+const double fwx = 3.125; //in mm
+const double fwy = 5.000; //in mm
+const double fSize = 200.0; //in mm
+
+
+
+Double_t get_mw12_pos_X(Double_t qmax, Int_t padmax, Double_t qleft, Double_t qright)
+{
+    Double_t a3 = TMath::Pi() * fwx / (TMath::ACosH(0.5 * (TMath::Sqrt(qmax / qleft) + TMath::Sqrt(qmax / qright))));
+    // Double_t a2 = gRandom->Uniform( -fwx / 2,    fwx / 2);
+    Double_t a2 = (a3 / TMath::Pi()) * TMath::ATanH((TMath::Sqrt(qmax / qleft) - TMath::Sqrt(qmax / qright)) /
+                                                    (2 * TMath::SinH(TMath::Pi() * fwx / a3)));
+
+    return (-1. * padmax * fwx + (fSize / 2) - (fwx / 2) - a2); // Left is positive and right negative
+}
+
+Double_t get_mw12_pos_Y(Double_t qmax, Int_t padmax, Double_t qdown, Double_t qup)
+{
+    Double_t a3 = TMath::Pi() * fwy / (TMath::ACosH(0.5 * (TMath::Sqrt(qmax / qdown) + TMath::Sqrt(qmax / qup))));
+    // Double_t a2 = gRandom->Uniform(-fwy / 2, fwy / 2);
+    Double_t a2 = (a3 / TMath::Pi()) * TMath::ATanH((TMath::Sqrt(qmax / qdown) - TMath::Sqrt(qmax / qup)) /
+                                                    (2 * TMath::SinH(TMath::Pi() * fwy / a3)));
+
+    return (padmax * fwy - (fSize / 2) + (fwy / 2) + a2);
+}
+
+//for mw3
+const double fSize_mw3_X = 900;
+const double fSize_mw3_Y = 600;
+Double_t get_mw3_pos_X(Double_t qmax, Int_t padmax, Double_t qleft, Double_t qright)
+{
+    Double_t a3 = TMath::Pi() * fwx / (TMath::ACosH(0.5 * (TMath::Sqrt(qmax / qleft) + TMath::Sqrt(qmax / qright))));
+    // Double_t a2 = gRandom->Uniform( -fwx / 2,    fwx / 2);
+    Double_t a2 = (a3 / TMath::Pi()) * TMath::ATanH((TMath::Sqrt(qmax / qleft) - TMath::Sqrt(qmax / qright)) /
+                                                    (2 * TMath::SinH(TMath::Pi() * fwx / a3)));
+
+    return (-1. * padmax * fwx + (fSize_mw3_X / 2) - (fwx / 2) - a2); // Left is positive and right negative
+}
+
+Double_t get_mw3_pos_Y(Double_t qmax, Int_t padmax, Double_t qdown, Double_t qup)
+{
+    Double_t a3 = TMath::Pi() * fwy / (TMath::ACosH(0.5 * (TMath::Sqrt(qmax / qdown) + TMath::Sqrt(qmax / qup))));
+    // Double_t a2 = gRandom->Uniform(-fwy / 2, fwy / 2);
+    Double_t a2 = (a3 / TMath::Pi()) * TMath::ATanH((TMath::Sqrt(qmax / qdown) - TMath::Sqrt(qmax / qup)) /
+                                                    (2 * TMath::SinH(TMath::Pi() * fwy / a3)));
+
+    return (padmax * fwy - (fSize_mw3_Y / 2) + (fwy / 2) + a2);
+}
